91b0245efb95b661c2a703ef74a08df2d9b3225b
purnima-poo <np03a190173@herladcollege.edu.np>
2021-07-19 10:44:49 +0545

    added js file

---
+++ index.js
@@ -0,0 +1,22 @@
+// 1.Ways to print in javascript
+//console.log("Hello World!");
+//alert("Wooh hooo!!");
+//document.write("This is document write")
+
+
+// 2.Javascript console api(application programming interface)
+// console.log("Hello World!", 4 + 6, "Another log");
+// console.warn("This is warning");
+// console.error("This is an error");
+
+/*multi
+ line 
+ comment */
+
+
+
+//3. Javascript variables
+//What are variables? - Containers to store data values
+var number1 = 2;
+var number2=6;
+console.log(number1+number2);
