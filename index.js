4d6aeeec38d95f988238d6d1d991c0eca5ecea7c
purnima-poo <np03a190173@herladcollege.edu.np>
2021-07-22 09:02:00 +0545

    added comparison operators

--- index.js
+++ index.js
@@ -81,3 +81,14 @@ var c = b;
 c *= 2;
 // c /= 2;
 console.log(c);
+
+
+//Comparison operator
+var x=34;
+ var y = 56;
+ console.log(x==y);
+ console.log(x>=y);
+ console.log(x!=y);
+ console.log(x<y);
+ console.log(x>y);
+ 
